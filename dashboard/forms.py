3ede18e5c8d574f0b937577bc8884aa32ed89988
bexruz <bexruzasqarov08@gmail.com>
2025-01-26 23:12:18 +0500

    completed

---
+++ dashboard/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from users.models import *
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = "__all__"
+        widgets = {
+            "title": forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = "__all__"
+        widgets = {
+            "title": forms.TextInput(attrs={'class': 'form-control'}),
+            "description": forms.TextInput(attrs={'class': 'form-control'}),
+            "cost": forms.TextInput(attrs={'class': 'form-control'}),
+            "price": forms.TextInput(attrs={'class': 'form-control'}),
+            "category": forms.Select(attrs={'class': 'form-control'}),
+            "image": forms.FileInput(attrs={'class': 'form-control', 'onchange': 'loadFile(event)'}),
+        }
+
+
+class CustomerForm(forms.ModelForm):
+    model = Customer
+    fields = "__all__"
+    widgets = {
+        "first_name": forms.TextInput(attrs={'class': 'form-control'}),
+        "last_name": forms.TextInput(attrs={'class': 'form-control'}),
+        "phone_number": forms.TextInput(attrs={'class': 'form-control'}),
+    }
