b2eef6800897d0be49cf811effee3423d3fcf6f5
bexruz <bexruzasqarov08@gmail.com>
2025-02-02 15:22:30 +0500

    from PostgreSQL to SQLite – for Deployment on a Free server

--- config/settings.py
+++ config/settings.py
@@ -77,14 +77,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'fastfood_db',
+#         'USER': 'fastfood_admin',
+#         'PASSWORD': 'fastfood',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fastfood_db',
-        'USER': 'fastfood_admin',
-        'PASSWORD': 'fastfood',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
